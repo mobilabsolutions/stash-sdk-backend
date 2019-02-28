eb49211112768286fa31b96ac40dcac9d38d3743
jovanamobilab <jovana@mobilabsolutions.com>
2019-02-28 17:31:12 +0100

    Added tests

--- payment-ws/src/test/kotlin/com/mobilabsolutions/payment/data/repository/AbstractRepositoryTest.kt
+++ payment-ws/src/test/kotlin/com/mobilabsolutions/payment/data/repository/AbstractRepositoryTest.kt
@@ -1,10 +1,13 @@
 package com.mobilabsolutions.payment.data.repository
 
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests
 
 /**
  * @author <a href="mailto:jovana@mobilabsolutions.com">Jovana Veskovic</a>
  */
 @ExtendWith(SpringExtension::class)
-class AbstractRepositoryTest
+@ContextConfiguration(locations = ["classpath:spring/dao-test.xml"])
+class AbstractRepositoryTest : AbstractTransactionalJUnit4SpringContextTests()
