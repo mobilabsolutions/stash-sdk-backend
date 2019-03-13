1f98684b8caf1081ead952877a31937b980acbef
jovanamobilab <jovana@mobilabsolutions.com>
2019-03-13 13:04:49 +0100

    Added BSPayone hashing; removed mocked data

---
+++ payment-bs-one/src/main/kotlin/com/mobilabsolutions/payment/bsone/service/BsPayoneHashingService.kt
@@ -0,0 +1,64 @@
+package com.mobilabsolutions.payment.bsone.service
+
+import com.google.common.base.Charsets
+import com.google.common.hash.Hashing
+import com.mobilabsolutions.payment.bsone.enum.BsPayoneRequestType
+import com.mobilabsolutions.payment.message.PspConfigModel
+import mu.KLogging
+import org.apache.tomcat.util.buf.HexUtils
+import org.springframework.stereotype.Service
+import java.nio.charset.StandardCharsets
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+/**
+ * @author <a href="mailto:jovana@mobilabsolutions.com">Jovana Veskovic</a>
+ */
+@Service
+class BsPayoneHashingService {
+    companion object : KLogging() {
+        const val API_VERSION = "3.11"
+        const val RESPONSE_TYPE = "JSON"
+        const val STORE_CARD_DATA_PARAM_VALUE = "yes"
+        const val HASH_ALGORITHM = "HmacSHA384"
+    }
+
+    /**
+     * Makes a hash for the client's credit card check request.
+     *
+     * @param pspConfigModel PSP Configuration Model
+     * @return hash
+     */
+    fun makeCreditCardCheckHash(pspConfigModel: PspConfigModel): String {
+        return calculateHash(pspConfigModel.key, pspConfigModel.accountId + API_VERSION + pspConfigModel.merchantId +
+            pspConfigModel.mode + pspConfigModel.portalId + BsPayoneRequestType.CREDIT_CARD_CHECK.type + RESPONSE_TYPE +
+            STORE_CARD_DATA_PARAM_VALUE)
+    }
+
+    /**
+     * Makes a key hash, which will be used for server requests.
+     *
+     * @param key Key to be hashed
+     * @return hashed key
+     */
+    fun hashKey(key: String): String {
+        return Hashing.md5().hashString(key, Charsets.UTF_8).toString()
+    }
+
+    private fun calculateHash(key: String?, data: String): String {
+        try {
+            val mac = Mac.getInstance(HASH_ALGORITHM)
+            val secretKeySpec = SecretKeySpec(key?.toByteArray(StandardCharsets.UTF_8), HASH_ALGORITHM)
+            mac.init(secretKeySpec)
+            return HexUtils.toHexString(mac.doFinal(data.toByteArray(StandardCharsets.UTF_8)))
+        } catch (e: NoSuchAlgorithmException) {
+            logger.error(e.message)
+            throw RuntimeException(e)
+        } catch (e: InvalidKeyException) {
+            logger.error(e.message)
+            throw RuntimeException(e)
+        }
+    }
+}
---
+++ payment-bs-one/src/main/kotlin/com/mobilabsolutions/payment/bsone/service/BsPayonePsp.kt
@@ -0,0 +1,39 @@
+package com.mobilabsolutions.payment.bsone.service
+
+import com.mobilabsolutions.payment.data.enum.PaymentServiceProvider
+import com.mobilabsolutions.payment.message.PspAliasConfigModel
+import com.mobilabsolutions.payment.message.PspConfigModel
+import com.mobilabsolutions.payment.service.psp.Psp
+import mu.KLogging
+import org.springframework.stereotype.Component
+
+/**
+ * @author <a href="mailto:doruk@mobilabsolutions.com">Doruk Coskun</a>
+ */
+@Component
+class BsPayonePsp(var bsPayoneHashingService: BsPayoneHashingService) : Psp {
+    companion object : KLogging() {
+        const val ENCODING = "UTF-8"
+    }
+
+    override fun getProvider(): PaymentServiceProvider {
+        return PaymentServiceProvider.BS_PAYONE
+    }
+
+    override fun calculatePspConfig(pspConfigModel: PspConfigModel?): PspAliasConfigModel? {
+        logger.info { "Random config calculation has been called..." }
+        return if (pspConfigModel != null) PspAliasConfigModel(
+            type = PaymentServiceProvider.BS_PAYONE.value,
+            merchantId = pspConfigModel.merchantId,
+            portalId = pspConfigModel.portalId,
+            apiVersion = BsPayoneHashingService.API_VERSION,
+            responseType = BsPayoneHashingService.RESPONSE_TYPE,
+            hash = bsPayoneHashingService.makeCreditCardCheckHash(pspConfigModel),
+            accountId = pspConfigModel.accountId,
+            encoding = ENCODING,
+            mode = pspConfigModel.mode,
+            publicKey = null,
+            privateKey = null
+        ) else null
+    }
+}
