70b7cd7a61916aad52ed607cc0b5db477af925a0
Mohamed Osman <mohamed.osman@mobilabsolutions.com>
2019-03-18 16:55:27 +0100

    Fixing comments

--- payment-ws/src/main/kotlin/com/mobilabsolutions/payment/model/MerchantRequestModel.kt
+++ payment-ws/src/main/kotlin/com/mobilabsolutions/payment/model/MerchantRequestModel.kt
@@ -2,6 +2,7 @@ package com.mobilabsolutions.payment.model
 
 import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
+import javax.validation.constraints.NotNull
 
 /**
  * @author <a href="mailto:mohamed.osman@mobilabsolutions.com">Mohamed Osman</a>
@@ -9,12 +10,15 @@ import io.swagger.annotations.ApiModelProperty
 @ApiModel(value = "Merchant Creation Request")
 data class MerchantRequestModel(
     @ApiModelProperty(value = "Merchant id", example = "Mobilab")
+    @field:NotNull
     val merchantId: String,
 
     @ApiModelProperty(value = "Merchant name", example = "Mobilab's Best Merchant")
+    @field:NotNull
     val merchantName: String,
 
     @ApiModelProperty(value = "Merchant email", example = "merchant@mobilabsolutions.com")
+    @field:NotNull
     val merchantEmail: String,
 
     @ApiModelProperty(value = "Merchant default currency", example = "EUR")
