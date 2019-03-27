5f11459eb33ee916969d5d003c13c5813e0cc238
Nikolaj <nikolaj@mobilabsolutions.com>
2019-03-27 17:24:31 +0100

    NO-TICKET - fixed misspelled property name.

--- payment-ws/src/main/kotlin/com/mobilabsolutions/payment/service/psp/PspRegistry.kt
+++ payment-ws/src/main/kotlin/com/mobilabsolutions/payment/service/psp/PspRegistry.kt
@@ -2,7 +2,6 @@ package com.mobilabsolutions.payment.service.psp
 
 import com.mobilabsolutions.payment.data.enum.PaymentServiceProvider
 import org.springframework.stereotype.Component
-import java.util.HashMap
 import javax.annotation.PostConstruct
 
 /**
@@ -10,7 +9,7 @@ import javax.annotation.PostConstruct
  */
 @Component
 class PspRegistry(private val psp: List<Psp>) {
-    private var pspMap: Map<PaymentServiceProvider, Psp> = HashMap()
+    private var pspMap: Map<PaymentServiceProvider, Psp> = mapOf()
 
     fun find(provider: PaymentServiceProvider): Psp? {
         return pspMap[provider]
