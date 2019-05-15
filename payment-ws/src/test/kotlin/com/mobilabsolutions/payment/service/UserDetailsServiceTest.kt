25a4a065acf81fd4286f59956b2bdf31b78131de
Mohamed Osman <mohamed.osman@mobilabsolutions.com>
2019-05-15 13:03:16 +0200

    Fix changes

--- payment-ws/src/test/kotlin/com/mobilabsolutions/payment/service/UserDetailsServiceTest.kt
+++ payment-ws/src/test/kotlin/com/mobilabsolutions/payment/service/UserDetailsServiceTest.kt
@@ -158,4 +158,14 @@ class UserDetailsServiceTest {
             )
         }
     }
+
+    @Test
+    fun `create already existing merchant user`() {
+        Assertions.assertThrows(ApiException::class.java) {
+            userDetailsService.createMerchantUser(
+                knownMerchant,
+                MerchantUserRequestModel(knownEmail, userPassword, "test name", "test lastname", "test locale")
+            )
+        }
+    }
 }
