a7bec0f14d57ab09cd6e48a20f558cffe68b75fa
Mohamed Osman <mohamed.osman@mobilabsolutions.com>
2019-06-18 14:30:20 +0200

    Remove extra comment

--- payment-commons/src/main/kotlin/com/mobilabsolutions/payment/data/enum/PaymentMethod.kt
+++ payment-commons/src/main/kotlin/com/mobilabsolutions/payment/data/enum/PaymentMethod.kt
@@ -1,11 +1,8 @@
 package com.mobilabsolutions.payment.data.enum
 
-import io.swagger.annotations.ApiModel
-
 /**
  * @author <a href="mailto:jovana@mobilabsolutions.com">Jovana Veskovic</a>
  */
-@ApiModel(value = "Payment methods: ")
 enum class PaymentMethod {
     CC,
     SEPA,
