efc1317895ac67c217ac137158b45e8592b226fe
Doruk Coskun <doruk@mobilabsolutions.com>
2019-06-21 11:50:46 +0200

    preparing travis configs for demo deployment

---
+++ k8s/gcp-demo-deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euox pipefail
+IFS=$'\n\t'
+
+source ./k8s/setDemoEnv.sh
+
+gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${REGION} --project ${PROJECT_ID}
+
+envsubst < k8s/resources/deployment.yaml | kubectl apply -f -
+envsubst < k8s/resources/configmap.yaml | kubectl apply -f -
+envsubst < k8s/resources/service.yaml | kubectl apply -f -
+envsubst < k8s/resources/ingress.yaml | kubectl apply -f -
