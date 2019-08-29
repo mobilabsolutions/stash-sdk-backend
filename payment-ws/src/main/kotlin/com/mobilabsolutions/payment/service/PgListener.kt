619861801a912a1698515585db126666be7214af
jovanamobilab <jovana@mobilabsolutions.com>
2019-08-29 18:14:36 +0200

    Refactoring after the code review

--- payment-ws/src/main/kotlin/com/mobilabsolutions/payment/service/PgListener.kt
+++ payment-ws/src/main/kotlin/com/mobilabsolutions/payment/service/PgListener.kt
@@ -5,8 +5,9 @@
 package com.mobilabsolutions.payment.service
 
 import com.mobilabsolutions.payment.data.repository.MerchantUserRepository
-import io.reactiverse.pgclient.PgClient
-import io.reactiverse.pgclient.PgPoolOptions
+import io.vertx.core.Vertx
+import io.vertx.pgclient.PgConnectOptions
+import io.vertx.pgclient.pubsub.PgSubscriber
 import mu.KLogging
 import org.json.JSONObject
 import org.springframework.beans.factory.annotation.Value
@@ -31,36 +32,34 @@ class PgListener(
 ) {
 
     companion object : KLogging() {
-        const val MERCHANT_ID = "merchant_id"
-        const val TRANSACTION_ID = "id"
+        private const val MERCHANT_ID = "merchant_id"
+        private const val TRANSACTION_ID = "id"
+        private const val POSTGRES_CHANNEL = "transaction_record"
+        private const val TOPIC_NAME = "/topic/transactions"
     }
 
     init {
-        val options = PgPoolOptions()
+        val connectionOptions = PgConnectOptions()
             .setPort(port.toInt())
             .setHost(host)
             .setDatabase(database)
             .setUser(username)
             .setPassword(password)
-        val client = PgClient.pool(options)
-        client.getConnection { ar1 ->
-            if (ar1.succeeded()) {
-                val connection = ar1.result()
-
-                connection.notificationHandler { notification ->
-                    run {
-                        logger.info { "Listening to live data." }
-                        val transactionNotification = JSONObject(notification.payload)
-                        val merchantUsers = merchantUserRepository.getMerchantUsers(transactionNotification.getString(MERCHANT_ID))
-                        merchantUsers.forEach { user ->
-                            simpleMessagingTemplate.convertAndSendToUser(user.email, "/topic/transactions",
-                                homeService.toLiveData(transactionNotification.getLong(TRANSACTION_ID)))
-                        }
+        val subscriber = PgSubscriber.subscriber(Vertx.vertx(), connectionOptions)
+        subscriber.connect { connection ->
+            if (connection.succeeded()) {
+                subscriber.channel(POSTGRES_CHANNEL).handler { payload ->
+                    logger.info { "Listening to live data." }
+                    println("Received $payload")
+                    val transactionNotification = JSONObject(payload)
+                    val merchantUsers = merchantUserRepository.getMerchantUsers(transactionNotification.getString(MERCHANT_ID))
+                    merchantUsers.forEach { user ->
+                        simpleMessagingTemplate.convertAndSendToUser(user.email, TOPIC_NAME, homeService.toLiveData(transactionNotification.getLong(TRANSACTION_ID)))
                     }
                 }
-
-                connection.query("LISTEN transaction_record") { logger.info { "Subscribed to the channel." } }
             }
         }
+
+        subscriber.reconnectPolicy { 5000 }
     }
 }
