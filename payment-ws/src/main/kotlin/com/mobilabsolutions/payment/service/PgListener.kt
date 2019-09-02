65eebd6702824aa75e53cd2ab0e56cf90bb11960
jovanamobilab <jovana@mobilabsolutions.com>
2019-09-02 09:16:56 +0200

    Edited logs

--- payment-ws/src/main/kotlin/com/mobilabsolutions/payment/service/PgListener.kt
+++ payment-ws/src/main/kotlin/com/mobilabsolutions/payment/service/PgListener.kt
@@ -49,15 +49,15 @@ class PgListener(
         subscriber.connect { connection ->
             if (connection.succeeded()) {
                 subscriber.channel(POSTGRES_CHANNEL).handler { payload ->
+                    logger.info { "Listening to live data." }
                     try {
-                        logger.info { "Listening to live data." }
                         val transactionNotification = JSONObject(payload)
                         val merchantUsers = merchantUserRepository.getMerchantUsers(transactionNotification.getString(MERCHANT_ID))
                         merchantUsers.forEach { user ->
                             simpleMessagingTemplate.convertAndSendToUser(user.email, TOPIC_NAME, homeService.toLiveData(transactionNotification.getLong(TRANSACTION_ID)))
                         }
                     } catch (exception: Exception) {
-                        logger.info("An error occurred while listening to live data. Message {}", exception.message)
+                        logger.error("An error occurred while listening to live data: {}", exception.message)
                     }
                 }
             }
